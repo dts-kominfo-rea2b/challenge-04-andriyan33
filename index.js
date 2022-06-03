55fa3847b683d7ce7463959cf06cfcddf87ba824
Acep Razif Andriyan <1177050002@student.uinsgd.ac.id>
2022-06-03 14:38:48 +0700

    Done Chalange

--- index.js
+++ index.js
@@ -8,7 +8,21 @@ const dates = [
 ];
 
 // TODO: Buatlah fungsi createDate
-const createDate = null;
+const createDate = (newData, index) => {
+  const sortDate = [];
+  for (let x = 0; x < newData.length; x++) {
+    const result = (new Date(newData[x]).getTime() / 1000).toString();
+    sortDate.push(result);
+  }
+
+  if (index === undefined) {
+    sortDate.sort((a, b) => a - b);
+    return sortDate.join(" - ");
+  } else {
+    sortDate.sort((a, b) => a + b);
+    return sortDate[index];
+  }
+};
 
 // ! JANGAN DIMODIFIKASI
 (() => {
